1d162c7285db8d4771257b0c6f45d94b09e7e73a
jordy-hamwijk <101179149+jordy-hamwijk@users.noreply.github.com>
2022-03-21 13:53:28 +0100

    Input field and button

--- src/main/java/com/giffing/wicket/spring/boot/example/web/pages/shop/CheeseShop.java
+++ src/main/java/com/giffing/wicket/spring/boot/example/web/pages/shop/CheeseShop.java
@@ -9,7 +9,6 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 
 import java.util.Arrays;
@@ -29,51 +28,43 @@ public class CheeseShop extends BasePage {
         WebMarkupContainer datacontainer = new WebMarkupContainer("data");
         datacontainer.setOutputMarkupId(true);
         add(datacontainer);
-        Form<Void> form = new Form<>("forms");
+        Form<Void> form = new Form<>("form");
 
         add(form);
         ListView<Cheese> listview = new ListView<Cheese>("items", soorten) {
             @Override
             protected void populateItem(ListItem<Cheese> item) {
 
+
                 item.add(new Label("soort", item.getModelObject().getSoortKaas()));
                 item.add(new Label("land", item.getModelObject().getLandHerkomst()));
                 item.add(new Label("gewicht", new PropertyModel<>(item.getModel(), "gewicht")));
-            }
 
-            //
+            }
         };
 
         datacontainer.add(listview);
 
         datacontainer.setVersioned(false);
-        //TODO Stap 3 Clean up code
-        final IModel<Cheese> soorten = new IModel<Cheese>() {
-            public String getValue() {
-                return value;
-            }
+        final TextField<String> field = new TextField<>("textInput");
 
-            @Override
-            public Cheese getObject() {
-                return null;
-            }
 
-            private String value;
-
-        };
-        Button button1 = new Button("button1") {
+        Button button = new Button("button") {
             @Override
             public void onSubmit() {
-                info("button1.onSubmit executed");
+
+                error("Login failed");
+
             }
         };
-        form.add(button1);
-        final TextField<Cheese> field = new TextField<Cheese>("ac", soorten) {
-            //TODO Stap 1 Textfield logica toevoegen
-            //TODO Stap 2 onSubmit logica toevoegen
-        };
-        form.add(field);
+        form.add(button, field);
 
+
+        // Stap 1  extra knop verwijderen
+        // Stap 2 Wicket id juist beneomen alle velden
+        //TODO Stap 3 Feedback panel toevoegen , als je op de op onSubmit drukt dat de info text word weergeven wordt. Zie voorbeeld Login.java
+        //TODO Stap 4 Als je tekst invoert en op de knop drukt, dit moet worden weergeven in de info feedback field.
+        //TODO Stap 5 Nieuwe kaas toevoegen aan de lijst met ingevoerde tekst. (AjaxSubmit)
     }
 }
 
