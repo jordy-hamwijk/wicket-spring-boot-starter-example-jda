9ffb240c14b85611554e13bfe1e8f2dc3a514565
jordy-hamwijk <101179149+jordy-hamwijk@users.noreply.github.com>
2022-03-24 13:51:44 +0100

    Add style.css to the CheeseShop

--- src/main/java/com/giffing/wicket/spring/boot/example/web/pages/home/HomePage.java
+++ src/main/java/com/giffing/wicket/spring/boot/example/web/pages/home/HomePage.java
@@ -5,10 +5,14 @@ import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 import com.giffing.wicket.spring.boot.example.web.pages.BasePage;
 import com.giffing.wicket.spring.boot.example.web.pages.customers.CustomerListPage;
 import com.giffing.wicket.spring.boot.example.web.pages.shop.CheeseShop;
+import com.giffing.wicket.spring.boot.example.web.pages.shop.TodoList;
 import com.giffing.wicket.spring.boot.example.web.pages.shop.oefenbook.GuestBook;
 import com.giffing.wicket.spring.boot.example.web.pages.websocket.ChatPage;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.request.resource.CssResourceReference;
 import org.wicketstuff.annotation.mount.MountPath;
 
 @WicketHomePage
@@ -21,5 +25,12 @@ public class HomePage extends BasePage {
 		add(new BookmarkablePageLink<String>("chatLink", ChatPage.class));
 		add(new BookmarkablePageLink<String>("KaasShop", CheeseShop.class));
 		add(new BookmarkablePageLink<String>("KaasBoek", GuestBook.class));
+		add(new BookmarkablePageLink<String>("TodoList", TodoList.class));
+
+	}
+
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		response.render(CssHeaderItem.forReference(new CssResourceReference(CheeseShop.class, "style.css")));
 	}
 }
